d5b9a3848284e98646d9010e974c9d3cb9385cda
Nur Laila <nurlailapcs03@gmail.com>
2022-05-20 13:26:15 +0700

    Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from"./component/Navbar";
+import Home from"./halaman/Home";
+import Surah from"./halaman/Surah";
+import Juz from"./halaman/Juz";
+import About from "./halaman/About";
+import {Route, Routes} from "react-router-dom";
+import React from "react";
+import InfoSurah from "./halaman/InfoSurah";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    return (
+        <>
+            <div className="App" >
+                <Navbar/>
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="surah/:id" element={<Surah/>}/>
+                    <Route path="juz/:id" element={<Juz/>}/>
+                    <Route path="info/:id" element={<InfoSurah/>}/>
+                    <Route path="/About" element={<About/>}/>
+                </Routes>
+            </div>
+        </>
   );
 }
 
